9e7572a7ad3fe41939e650720b5961c4c116b312
Yaser <yaserim@duck.com>
2025-05-31 03:22:23 +0330

    fix: Add icon file from Studio repository

--- build.sh
+++ build.sh
@@ -39,6 +39,9 @@ StartupWMClass=Studio
 MimeType=text/html;
 EOL
 
+# Copy icon to AppDir root
+cp assets/studio-app-icon.png AppDir/com.automattic.Studio.png
+
 cat > AppDir/AppRun << EOL
 #!/bin/bash
 SELF=$(readlink -f "$0")
